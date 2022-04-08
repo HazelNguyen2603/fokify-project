c8446e1aa3095f8bc022d0ab49a73f09726aefd5
HazelNguyen2603 <nhatnguyena@gmail.com>
2022-04-08 20:45:01 +0700

    Clean up controler

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,10 +124,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipe);
@@ -137,7 +133,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResult);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 // window.addEventListener('hashchange', controlRecipe);
